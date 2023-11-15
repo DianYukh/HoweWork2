4dc338f8eeadf86e1f5b3f2a09be37963e9016cd
DianYukh <dianayukhymukua@gmail.com>
2023-11-15 10:48:23 +0100

    initial commit

--- src/main/java/org/example/Homework/homework2122/Menu.java
+++ src/main/java/org/example/Homework/homework2122/Menu.java
@@ -27,6 +27,7 @@ public class Menu {
                 CoffeeMachine selectMachine = null;
                 boolean isSelectMachine = false;
 
+
                 //обрати кавомашину
                 switch (machineChoice) {
                     case 1 -> {
@@ -45,39 +46,47 @@ public class Menu {
 
 
                 if (isSelectMachine) {
-
-                    //обрати операцію
-                    System.out.println("Оберіть операцію");
-                    System.out.println("1 - Увімкнути");
-                    System.out.println("2 - Вимкнути");
-                    System.out.println("3 - Еспрессо");
-                    System.out.println("4 - Амерікано");
-                    System.out.println("5 - Додати воду");
-                    System.out.println("6 - Додати каву");
-                    System.out.println("7 - Очистити бак");
-                    System.out.println("0 - Вихід");
-
-
-                    //=== 3 додати обробку InputMismatchException через try catch ===
-                    //=== 4 додати цикл для можливості повтору меню ===
-                    int choice = scanner.nextInt();
+                    boolean repeatOperation = true;
+                    while (repeatOperation) {
+                        try {
+                            //обрати операцію
+                            System.out.println("Оберіть операцію");
+                            System.out.println("1 - Увімкнути");
+                            System.out.println("2 - Вимкнути");
+                            System.out.println("3 - Еспрессо");
+                            System.out.println("4 - Амерікано");
+                            System.out.println("5 - Додати воду");
+                            System.out.println("6 - Додати каву");
+                            System.out.println("7 - Очистити бак");
+                            System.out.println("0 - Вихід");
 
 
-                    //=== 5 пропищіть виклик всіх необхідних методів ===
-                    switch (choice) {
-                        case 1 -> selectMachine.turnOn();
-                        case 2 -> selectMachine.turnOff();
-                        case 3 -> selectMachine.makeEspresso();
-                        case 4 -> selectMachine.makeAmericano();
-                        case 5 -> selectMachine.addWater();
-                        case 6 -> selectMachine.addCoffee();
-                        case 7 -> selectMachine.cleanGarbageBox();
-                        case 0 -> {
-                            System.out.println("Good bye");
-                            repeatMenu = false;
+                            //=== 3 додати обробку InputMismatchException через try catch ===
+                            //=== 4 додати цикл для можливості повтору меню ===
+                            int choice = scanner.nextInt();
+
+
+                            //=== 5 пропищіть виклик всіх необхідних методів ===
+                            switch (choice) {
+                                case 1 -> selectMachine.turnOn();
+                                case 2 -> selectMachine.turnOff();
+                                case 3 -> selectMachine.makeEspresso();
+                                case 4 -> selectMachine.makeAmericano();
+                                case 5 -> selectMachine.addWater();
+                                case 6 -> selectMachine.addCoffee();
+                                case 7 -> selectMachine.cleanGarbageBox();
+                                case 0 -> {
+                                    System.out.println("Good bye");
+                                    repeatMenu = false;
+                                    repeatOperation = false;
+                                }
+
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println("Помилка введення операції. Повторіть спробу.");
+                            scanner.nextLine();
                         }
                     }
-
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Помилка введення. Повторіть спробу.");
